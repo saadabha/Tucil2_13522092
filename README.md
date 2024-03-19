9b1c1bba3f11fe796b09daccc60fbaee8c3a7a80
saadabha <saadabdulhakim08@gmail.com>
2024-03-19 11:48:26 +0700

    feat: add execution time, test, readme, and doc

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Tucil2_13522092
+# Membangun Kurva Bézier dengan Algoritma Titik Tengah berbasis Divide and Conquer
+Tugas Kecil 2 Strategi Algoritma
+
+## Deskripsi Program
+Program ini bertujuan untuk membuat Kurva Bezier Kuadratik dengan menggunakan dua pendekatan berbeda yaitu algoritma titik tengah berbasis divide and conquer serta algoritma bruteforce dan membandingkan keduanya
+
+## Requirement Program
+- Python 3.x
+- Library Matplotlib untuk visualisasi grafis (instal dengan `pip install matplotlib`)
+
+## Cara Menjalankan dan Menggunakan program
+1. Siapkan file dan letakkan dalam folder "test" jika Anda ingin menggunakan input dari file.
+2. Format input file adalah tiga baris pertama berisi koordinat titik P0, P1, dan P2 dalam format `x y`, diikuti oleh jumlah iterasi yang diinginkan pada baris terakhir.
+3. Jalankan file `main.py` menggunakan Python.
+4. Program akan meminta Anda memilih metode input yang diinginkan: melalui file atau secara manual.
+5. Jika Anda memilih input melalui file, masukkan nama file dengan format yang benar saat diminta.
+6. Jika memilih input manual, masukkan koordinat titik P0, P1, P2, dan jumlah iterasi secara berurutan di terminal.
+7. Tekan tombol "Enter" dan program akan berjalan. 
+8. Program kemudian akan menampilkan hasil grafik dari kedua algoritma dalam jendela baru.
+
+## Identitas Pembuat
+Sa'ad Abdul Hakim
+13522092
--- src/main.py
+++ src/main.py
@@ -6,12 +6,34 @@ import time
 # while jumlahpoint < 3:
 #     jumlahpoint = int(input("Masukkan jumlah point(minimal 3): "))
 
-pointlist = createallinitialpoint()
+print("Silahkan pilih cara input:")
+print("1. Input manual")
+print("2. Input file")
+masukkan = int(input("Pilihan: "))
 
-iteration = int(input("Masukkan jumlah iterasi: "))
-while iteration < 1:
-    print("Jumlah iterasi minimal 1")
-    iteration = int(input("Masukkan jumlah iterasi(minimal 1): "))
+if masukkan == 1:
+    pointlist = createallinitialpoint()
+
+    iteration = int(input("Masukkan jumlah iterasi: "))
+    while iteration < 1:
+        print("Jumlah iterasi minimal 1")
+        iteration = int(input("Masukkan jumlah iterasi(minimal 1): "))
+elif masukkan == 2:
+    filename = input("Masukkan nama file: ")
+    filename = "../test/"+filename
+    with open(filename, "r") as file:
+        pointlist = []
+        
+        for i in range(3):
+            line = file.readline()
+            numbers = line.split()
+            numbers_tuple = tuple(float(num) for num in numbers)
+            pointlist.append(numbers_tuple)
+        val = file.readline()
+        iteration = int(val)
+else:
+    print("Input tidak valid")
+    exit()
 
 plt.subplot(1, 2, 1)
 
@@ -26,14 +48,14 @@ midpointbeziercurve = createbezier(pointlist, iteration)
 endmidpoint = time.perf_counter()
 decimal_places = 4
 # exectimemidpoint = f"{endmidpoint-startmidpoint:.{decimal_places}f}"
-exectimemidpoint = endmidpoint-startmidpoint
+exectimemidpoint = (endmidpoint-startmidpoint)*1000
 
 finalcoorx = [point[0] for point in midpointbeziercurve]
 finalcoory = [point[1] for point in midpointbeziercurve]
 
 plt.plot(finalcoorx, finalcoory, marker='o', linestyle='-', label='Final Bezier Curve')
 
-titlemidpoint = "Midpoint Algorithm (Execution Time: "+str(exectimemidpoint)+" s)"
+titlemidpoint = "Midpoint Algorithm (Execution Time: "+str(exectimemidpoint)+" ms)"
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title(titlemidpoint)
@@ -48,14 +70,14 @@ startbruteforce = time.perf_counter()
 bruteforcebeziercurve = bruteforcebezier(pointlist, iteration)
 endbruteforce = time.perf_counter()
 # exectimebruteforce = f"{endbruteforce-startbruteforce:.{decimal_places}f}"
-exectimebruteforce = endbruteforce-startbruteforce
+exectimebruteforce = (endbruteforce-startbruteforce)*1000
 
 coorx = [point[0] for point in bruteforcebeziercurve]
 coory = [point[1] for point in bruteforcebeziercurve]
 
 plt.plot(coorx, coory, marker='o', linestyle='-', label='Final Bezier Curve')
 
-titlebruteforce = "Bruteforce Algorithm (Execution Time: "+str(exectimebruteforce)+" s)"
+titlebruteforce = "Bruteforce Algorithm (Execution Time: "+str(exectimebruteforce)+" ms)"
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title(titlebruteforce)
@@ -64,6 +86,4 @@ plt.grid()
 
 plt.suptitle('Bezier Curve')
 
-print(exectimemidpoint)
-print(exectimebruteforce)
 plt.show()
